697bb491b9de4f64e6e3c7e6e08cceaef4d231a5
Vinicius-Jose <vinijosenog@hotmail.com>
2018-09-21 18:50:27 -0300

    Inicio do projeto

---
+++ EntertainmentWishList/src/controller/CabecalhoController.java
@@ -0,0 +1,63 @@
+package controller;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.Writer;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Enumeration;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@RequestScoped
+@ManagedBean
+public class CabecalhoController {
+
+	public String getTxt() {
+		ExternalContext ex = FacesContext.getCurrentInstance().getExternalContext();
+
+		return "jhfdshflkjsahdfklsaj";
+
+	}
+
+	public void setTxt(String txt) {
+		ExternalContext ex = FacesContext.getCurrentInstance().getExternalContext();
+		HttpSession s = (HttpSession) ex.getSession(true);
+		s.setAttribute("txt", txt);
+	}
+
+	public void click() {
+		HttpServletRequest req = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext()
+				.getRequest();
+		Enumeration<String> as = req.getAttributeNames();
+		while (as.hasMoreElements()) {
+			System.out.println(as.nextElement());
+
+		}
+		ExternalContext ex = FacesContext.getCurrentInstance().getExternalContext();
+		HttpSession s = (HttpSession) ex.getSession(true);
+		System.out.println(s.getAttribute("txt"));
+
+	}
+
+	public URL getImage() throws IOException {
+
+		try {
+			return new URL(
+					"https://upload.wikimedia.org/wikipedia/pt/thumb/3/37/Capa_de_Kamikaze_%28Eminem%29.jpg/220px-Capa_de_Kamikaze_%28Eminem%29.jpg");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
